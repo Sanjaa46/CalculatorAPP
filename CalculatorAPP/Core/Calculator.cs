89ec663ad186d6e5b29b97f6b74e69e5944c69d3
Sanjaa46 <sanjaas880@gmail.com>
2025-02-17 07:51:37 +0800

    First commit to Lab 1

---
+++ CalculatorAPP/Core/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorApp.Models;
+using CalculatorApp.Interfaces;
+
+namespace CalculatorApp.Core
+{
+    public class Calculator : CalculatorBase, ICalculations
+    {
+        public void Add(double number)
+        {
+            Result += number;
+        }
+
+        public void Subtract(double number)
+        {
+            Result -= number;
+        }
+    }
+}
+
--- CalculatorAPP/Program.cs
+++ CalculatorAPP/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using CalculatorApp.Core;
+using CalculatorApp.Models;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            Memory memory = new Memory();
+
+            calc.Add(10);
+            Console.WriteLine($"Result after adding 10: {calc.Result}");
+
+            calc.Subtract(3);
+            Console.WriteLine($"Result after subtracting 3: {calc.Result}");
+
+            memory.Store(calc.Result);
+            Console.WriteLine("Stored result in memory.");
+
+            double recalled = memory.Recall(0);
+            Console.WriteLine($"Recalled from memory: {recalled}");
+        }
+    }
+}
