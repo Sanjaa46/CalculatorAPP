2d455fb74ca4c387464c012f24bef49f3fe89df6
Sanjaa46 <sanjaas880@gmail.com>
2025-02-24 17:02:49 +0800

    windows lab2

---
+++ CalculatorLibrary/Calculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CalculatorLibrary
+{
+    /// <summary>
+    /// A simple calculator that performs addition and subtraction.
+    /// </summary>
+    public class Calculator : CalculatorBase, ICalculations
+    {
+        /// <summary>
+        /// Adds a number to the result.
+        /// </summary>
+        /// <param name="number">The number to add.</param>
+        public void Add(double number)
+        {
+            Result += number;
+        }
+
+        /// <summary>
+        /// Subtracts a number from the result.
+        /// </summary>
+        /// <param name="number">The number to subtract.</param>
+        public void Subtract(double number)
+        {
+            Result -= number;
+        }
+    }
+
+    /// <summary>
+    /// Abstract class for a calculator. Stores calculation results.
+    /// </summary>
+    public abstract class CalculatorBase
+    {
+        /// <summary>
+        /// Stores the result of calculations.
+        /// </summary>
+        public double Result { get; set; }
+    }
+
+    /// <summary>
+    /// Interface for basic calculations.
+    /// </summary>
+    public interface ICalculations
+    {
+        /// <summary>
+        /// Adds a number to the result.
+        /// </summary>
+        /// <param name="number">The number to add.</param>
+        void Add(double number);
+
+        /// <summary>
+        /// Subtracts a number from the result.
+        /// </summary>
+        /// <param name="number">The number to subtract.</param>
+        void Subtract(double number);
+    }
+}
